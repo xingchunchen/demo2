97a59c2a0cac5ceeb3d0532d79c646c10578e51d
Charlie Chen <chchen@expediagroup.com>
2021-10-15 17:31:03 -0700

    dev1

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo2Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Demo2Application.class, args);
+        System.out.println("dev");
     }
 
 }
